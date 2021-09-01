d88e9032f492bcdcfd5124a8edd5e7b1db6dd8db
Luciano Alvarez <luchoalvarez@gmail.com>
2021-09-01 19:22:29 -0300

    logging handler to stdout for cron and logic from multiple AP MACs

--- main.py
+++ main.py
@@ -10,8 +10,10 @@ from cloud.big_query import BigQueryProcessor
 from local_db.local_db import SQLite
 # import UDM.unify as controller
 import UDM_PRO.unify as controller
+import sys
 
-load_dotenv(f'{os.path.dirname(os.path.realpath(__file__))}/.env')
+script_path =os.path.dirname(os.path.realpath(__file__))
+load_dotenv(f'{script_path}/.env')
 urllib3.disable_warnings()
 UNIFI_USER = os.getenv('USERNAME')
 UNIFI_PWD = os.getenv('PASSWORD')
@@ -22,12 +24,18 @@ DEST_TABLE_SCHEMA = {'name': 'user', 'type': 'STRING', 'mode': 'REQUIRED'}, \
                     {'name': 'spent', 'type': 'INTEGER', 'mode': 'NULLABLE'}, \
                     {'name': 'date', 'type': 'DATE', 'mode': 'REQUIRED'}, \
                     {'name': 'uuid', 'type': 'STRING', 'mode': 'REQUIRED'}
-SVC_ACCOUNT = f'{os.path.dirname(os.path.realpath(__file__))}/files/svc_account_occupancy.json'
-DEST_JSON_FILE = f'{os.path.dirname(os.path.realpath(__file__))}/files/bq_user_updates.json'
-DAILY_STATS_FILE = f'{os.path.dirname(os.path.realpath(__file__))}/files/daily_stats.json'
+SVC_ACCOUNT = f'{script_path}/files/svc_account_occupancy.json'
+DEST_JSON_FILE = f'{script_path}/files/bq_user_updates.json'
+DAILY_STATS_FILE = f'{script_path}/files/daily_stats.json'
 
-logging.basicConfig(filename='automation.log', level=logging.INFO,
+root = logging.getLogger()
+root.setLevel(logging.DEBUG)
+handler = logging.StreamHandler(sys.stdout)
+logging.basicConfig(filename='automation.txt', level=logging.INFO,
                     format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
+handler.setFormatter(formatter)
+root.addHandler(handler)
 
 
 class OccupancyServer:
@@ -48,13 +56,16 @@ class OccupancyServer:
 
     def load_status_file(self):
         """Updates local file based on current and unifi stats"""
-        logging.info("Updating JSON file...")
+        logging.info("Updating daily stats data...")
         for user, data in self.unifi_stats.items():
-            self.current_stats[user] = data
+            self.current_stats[user] = {
+                "spent": sum(data.values()),
+                "date": datetime.today().strftime('%Y-%m-%d')
+            }
         with open(self.daily_stats_file, 'w') as f:
             f.write(json.dumps(self.current_stats, indent=4))
         f.close()
-        logging.info("JSON file updated")
+        # logging.info("JSON file updated")
 
     def restart_daily_stats(self):
         with open(self.daily_stats_file, 'w') as f:
@@ -81,18 +92,16 @@ class OccupancyServer:
             device_list = api.list_clients()
         users_data = {}
         excluded = ['IPHONE', 'GALAXY', 'SAMSUNG', 'ONEPLUS', 'HUB', 'RASPBERRYPI', 'NUKI_BRIDGE_201F6482',
-                    'FENIX-BCN-PRINTER', 'DESKTOP', 'XIAOMI']
+                    'FENIX-BCN-PRINTER', 'DESKTOP', 'XIAOMI', 'POCOPHON']
         for client in device_list:
-            if 'hostname' in client:
+            if 'hostname' in client and 'ap_mac' in client:
                 hostname = client['hostname'].upper()
                 is_excluded = any(device in hostname for device in excluded)
                 if is_excluded:
                     continue
                 clean_data = {
-                    client['hostname']:
-                        {
-                        'spent': ceil((client['last_seen'] - client['assoc_time']) / 60),
-                        'date': datetime.today().strftime('%Y-%m-%d')
+                    client['hostname']:{
+                        client['ap_mac']: ceil((client['last_seen'] - client['assoc_time']) / 60)
                     }
                 }
                 users_data.update(clean_data)
@@ -106,7 +115,7 @@ def initial_setup():
 
 
 if __name__ == "__main__":
-    logging.info('Starting Job...')
+    # logging.info('Starting Job...')
     # initial_setup()
     server = OccupancyServer()
     server.get_unify_data()
@@ -114,13 +123,14 @@ if __name__ == "__main__":
     # result = runner.run_query('select * from local')
     # for row in result:
     #     print(row)
-    if datetime.today().hour == 19 and datetime.today().minute > 40:  # last cron job is at 19:45
+    if datetime.today().hour == 19 and datetime.today().minute >= 45:
         bq_values = server.get_biq_query_rows()
         values = server.get_sql_rows()
         server.restart_daily_stats()
         bq_runner = BigQueryProcessor(SVC_ACCOUNT, DEST_PROJECT_ID, DEST_DATASET_ID,
                                       DEST_TABLE_ID, DEST_TABLE_SCHEMA, DEST_JSON_FILE)
         bq_runner.create_json_newline_delimited_file(bq_values)
+        logging.info(f'Values: {bq_values}')
         bq_runner.load_data_from_file()
         db_runner = SQLite()
         db_runner.load_local_db(values)
